b0be876599a2c3845faaa669750c3ab994efaac3
Erik Kratochvil <rx@RX-MacBook-Air.local>
2022-01-24 21:01:37 +0100

    Initial version

---
+++ src/test/java/org/demo/AppTest.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertThat(true).isTrue();
+    }
+}
