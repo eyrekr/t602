ef7a96b33863dee3e7d4f3c63842b1c5454c38da
Erik <erik.kratochvil@outlook.cz>
2022-01-25 14:18:06 +0100

    Add manifest

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-mvn clean package
-mv target/t602-1.0-SNAPSHOT.jar target/t602.jar
-java -jar target/t602.jar
+java -jar target/t602-1.0-SNAPSHOT.jar
+
--- src/main/java/org/demo/effects/FpsCounter.java
+++ src/main/java/org/demo/effects/FpsCounter.java
@@ -4,15 +4,22 @@ import org.demo.core.Layer;
 
 public class FpsCounter implements Effect {
 
-    private long lastTick = System.nanoTime();
+    private long lastTime = System.nanoTime();
+    private int[] durations = new int[32];
+    private int i = 0;
 
     @Override
     public Lifecycle apply(final Layer layer) {
-        final int timeToRenderOneFrameInMillis = (int) ((System.nanoTime() - lastTick) / 1_000_000L);
-        lastTick = System.nanoTime();
-        final int fps = timeToRenderOneFrameInMillis > 0 ? 1_000 / timeToRenderOneFrameInMillis : 0;
-        final String text = fps +" fps";
+        final int durationInMillis = (int) ((System.nanoTime() - this.lastTime) / 1_000_000L);
+        this.lastTime = System.nanoTime();
+        this.durations[this.i] = durationInMillis;
+        this.i = (this.i + 1) % this.durations.length;
 
+        int sum = 0;
+        for (int i = 0; i < this.durations.length; i++) {
+            sum += this.durations[i];
+        }
+        final String text = (sum / this.durations.length) + "ms";
         layer.put(layer.width - text.length(), 0, text);
 
         return Lifecycle.Active;
